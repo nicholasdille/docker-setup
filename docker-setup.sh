6d1da85218fb359c455f5f4dec71c8bc7d6a31f1
Nicholas Dille <nicholas.dille@mailbox.org>
2021-10-29 14:03:39 +0200

    Added renovate

--- docker-setup.sh
+++ docker-setup.sh
@@ -51,6 +51,7 @@ function log() {
 # Install Docker CE
 # TODO: Support rootless?
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
+# renovate: datasource=github-releases depName=moby/moby
 DOCKER_VERSION=20.10.8
 if ! apt -qq list --installed docker-ce 2>/dev/null | grep --quiet docker-ce; then
     log "Install Docker Engine ${DOCKER_VERSION}"
@@ -92,7 +93,9 @@ mkdir -p "${TARGET}/libexec/docker/cli-plugins"
 # docker-compose v2
 # TODO: Set target directory for non-root
 : "${DOCKER_COMPOSE:=v2}"
+# renovate: datasource=github-releases depName=docker/compose versioning=regex:^(?<major>1)\.(?<minor>\d+)\.(?<patch>\d+)$
 DOCKER_COMPOSE_VERSION_V1=1.29.2
+# renovate: datasource=github-releases depName=docker/compose
 DOCKER_COMPOSE_VERSION_V2=2.0.0
 log "Install docker-compose ${DOCKER_COMPOSE} (${DOCKER_COMPOSE_VERSION_V1} or ${DOCKER_COMPOSE_VERSION_V2})"
 DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION_V2}/docker-compose-linux-amd64"
@@ -112,61 +115,73 @@ EOF
 fi
 
 # docker-scan
+# renovate: datasource=github-releases depName=docker/scan-cli-plugin
 DOCKER_SCAN_VERSION=0.8.0
 log "Install docker-scan ${DOCKER_SCAN_VERSION}"
 curl -sLo "${TARGET}/libexec/docker/cli-plugins/docker-scan" "https://github.com/docker/scan-cli-plugin/releases/download/v${DOCKER_SCAN_VERSION}/docker-scan_linux_amd64"
 chmod +x "${TARGET}/libexec/docker/cli-plugins/docker-scan"
 
 # hub-tool
+# renovate: datasource=github-releases depName=docker/hub-tool
 HUB_TOOL_VERSION=0.4.3
 log "Install hub-tool ${HUB_TOOL_VERSION}"
 curl -sL "https://github.com/docker/hub-tool/releases/download/v${HUB_TOOL_VERSION}/hub-tool-linux-amd64.tar.gz" | tar -xzC "${TARGET}/bin" --strip-components=1
 
 # docker-machine
+# renovate: datasource=github-releases depName=docker/machine
 DOCKER_MACHINE_VERSION=0.16.2
 log "Install docker-machine ${DOCKER_MACHINE_VERSION}"
 curl -sLo "${TARGET}/bin/docker-machine" "https://github.com/docker/machine/releases/download/v${DOCKER_MACHINE_VERSION}/docker-machine-Linux-x86_64"
 chmod +x "${TARGET}/bin/docker-machine"
 
 # buildx
+# renovate: datasource=github-releases depName=docker/buildx
 BUILDX_VERSION=0.6.3
 log "Install buildx ${BUILDX_VERSION}"
 curl -sLo "${TARGET}/libexec/docker/cli-plugins/docker-buildx" "https://github.com/docker/buildx/releases/download/v${BUILDX_VERSION}/buildx-v${BUILDX_VERSION}.linux-amd64"
 chmod +x "${TARGET}/libexec/docker/cli-plugins/docker-buildx"
 
 # manifest-tool
+# renovate: datasource=github-releases depName=estesp/manifest-tool
 MANIFEST_TOOL_VERSION=1.0.3
 log "Install manifest-tool ${MANIFEST_TOOL_VERSION}"
 curl -sLo "${TARGET}/bin/manifest-tool" "https://github.com/estesp/manifest-tool/releases/download/v${MANIFEST_TOOL_VERSION}/manifest-tool-linux-amd64"
 chmod +x "${TARGET}/bin/manifest-tool"
 
 # BuildKit
+# renovate: datasource=github-releases depName=moby/buildkit
 BUILDKIT_VERSION=0.9.1
 log "Install BuildKit ${BUILDKIT_VERSION}"
 curl -sL "https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-amd64.tar.gz" | tar -xzC "${TARGET}"
 
 # img
+# renovate: datasource=github-releases depName=genuinetools/img
 IMG_VERSION=0.5.11
 log "Install img ${IMG_VERSION}"
 curl -sLo "${TARGET}/bin/img" "https://github.com/genuinetools/img/releases/download/v${IMG_VERSION}/img-linux-amd64"
 chmod +x "${TARGET}/bin/img"
 
 # dive
+# renovate: datasource=github-releases depName=wagoodman/dive
 DIVE_VERSION=0.10.0
 log "Install dive ${DIVE_VERSION}"
 curl -sL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.tar.gz | tar -xzC "${TARGET}/bin" dive
 
 # TODO: portainer
+# renovate: datasource=github-releases depName=portainer/portainer
+PORTAINER_VERSION=2.9.0
 # https://github.com/portainer/portainer/releases/download/2.9.0/portainer-2.9.0-linux-amd64.tar.gz
 # portainer/portainer
 # portainer/public/
 
 # oras
+# renovate: datasource=github-releases depName=oras-project/oras
 ORAS_VERSION=0.12.0
 log "Install oras ${ORAS_VERSION}"
 curl -sL "https://github.com/oras-project/oras/releases/download/v${ORAS_VERSION}/oras_${ORAS_VERSION}_linux_amd64.tar.gz" | tar -xzC "${TARGET}/bin" oras
 
 # regclient
+# renovate: datasource=github-releases depName=regclient/regclient
 REGCLIENT_VERSION=0.3.8
 log "Install regclient ${REGCLIENT_VERSION}"
 curl -sLo "${TARGET}/bin/regctl"  "https://github.com/regclient/regclient/releases/download/v${REGCLIENT_VERSION}/regctl-linux-amd64"
@@ -179,24 +194,28 @@ chmod +x "${TARGET}/bin/regsync"
 # Kubernetes
 
 # kubectl
+# renovate: datasource=github-releases depName=kubernetes/kubernetes
 KUBECTL_VERSION=1.22.0
 log "Install kubectl ${KUBECTL_VERSION}"
 curl -sLo "${TARGET}/bin/kubectl" "https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 chmod +x "${TARGET}/bin/kubectl"
 
 # kind
+# renovate: datasource=github-releases depName=kubernetes-sigs/kind
 KIND_VERSION=0.11.1
 log "Install kind ${KIND_VERSION}"
 curl -sLo "${TARGET}/bin/kind" "https://github.com/kubernetes-sigs/kind/releases/download/v${KIND_VERSION}/kind-linux-amd64"
 chmod +x "${TARGET}/bin/kind"
 
 # k3d
+# renovate: datasource=github-releases depName=rancher/k3d
 K3D_VERSION=4.4.8
 log "Install k3d ${K3D_VERSION}"
 curl -sLo "${TARGET}/bin/k3d" "https://github.com/rancher/k3d/releases/download/v${K3D_VERSION}/k3d-linux-amd64"
 chmod +x "${TARGET}/bin/k3d"
 
 # helm
+# renovate: datasource=github-releases depName=helm/helm
 HELM_VERSION=3.7.0
 log "Install helm ${HELM_VERSION}"
 curl -sL "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz" | tar -xzC "${TARGET}/bin" --strip-components=1 linux-amd64/helm
@@ -205,23 +224,27 @@ curl -sL "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz" | tar -x
 # https://krew.sigs.k8s.io/docs/user-guide/setup/install/
 
 # kustomize
+# renovate: datasource=github-releases depName=kubernetes-sigs/kustomize
 log "Install kustomize ${KUSTOMIZE_VERSION}"
 KUSTOMIZE_VERSION=4.4.0
 curl -sL "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz" | tar -xzC "${TRARGET}/bin"
 
 # kapp
+# renovate: datasource=github-releases depName=vmware-tanzu/carvel-kapp
 KAPP_VERSION=0.42.0
 log "Install kapp ${KAPP_VERSION}"
 curl -sLo "${TARGET}/bin/kapp" "https://github.com/vmware-tanzu/carvel-kapp/releases/download/v${KAPP_VERSION}/kapp-linux-amd64"
 chmod +x "${TARGET}/bin/kapp"
 
 # ytt
+# renovate: datasource=github-releases depName=vmware-tanzu/carvel-ytt
 YTT_VERSION=0.37.0
 log "Install ytt ${YTT_VERSION}"
 curl -sLo "${TARGET}/bin/ytt" "https://github.com/vmware-tanzu/carvel-ytt/releases/download/v${YTT_VERSION}/ytt-linux-amd64"
 chmod +x "${TARGET}/bin/ytt"
 
 # arkade
+# renovate: datasource=github-releases depName=alexellis/arkade
 ARKADE_VERSION=0.8.8
 log "Install arkade ${ARKADE_VERSION}"
 curl -sLo "${TARGET}/bin/arkade" "https://github.com/alexellis/arkade/releases/download/${ARKADE_VERSION}/arkade"
@@ -230,6 +253,7 @@ chmod +x "${TARGET}/bin/arkade"
 # Security
 
 # trivy
+# renovate: datasource=github-releases depName=aquasecurity/trivy
 TRIVY_VERSION=0.19.2
 log "Install trivy ${TRIVY_VERSION}"
 curl -sL "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.tar.gz" | tar -xzC "${TARGET}/bin" trivy
@@ -237,12 +261,14 @@ curl -sL "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSI
 # Tools
 
 # jq
+# renovate: datasource=github-releases depName=stedolan/jq
 JQ_VERSION=1.6
 log "Install jq ${JQ_VERSION}"
 curl -sLo "${TARGET}/bin/jq" "https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64"
 chmod +x "${TARGET}/bin/jq"
 
 # yq
+# renovate: datasource=github-releases depName=mikefarah/yq
 YQ_VERSION=4.13.2
 log "Install yq ${YQ_VERSION}"
 curl -sLo "${TARGET}/bin/yq" "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_amd64"
