c4ec3b180eacd36d4ced03a02ef500963c23b9cf
Nicholas Dille <nicholas.dille@mailbox.org>
2021-11-29 22:36:21 +0100

    Fixed check for BuildKit in daemon

--- docker-setup.sh
+++ docker-setup.sh
@@ -713,7 +713,7 @@ if ${INSTALL_DOCKER} || ${REINSTALL}; then
         DOCKER_RESTART=true
         echo -e "${YELLOW}WARNING: Docker will be restarted later unless DOCKER_ALLOW_RESTART=false.${RESET}"
     fi
-    if ! jq --raw-output '.features.buildkit // false' /etc/docker/daemon.json >/dev/null; then
+    if ! "$(jq --raw-output '.features.buildkit // false' /etc/docker/daemon.json >/dev/null)"; then
         task "Enable BuildKit"
         # shellcheck disable=SC2094
         cat <<< "$(jq '. * {"features":{"buildkit":true}}' /etc/docker/daemon.json)" >/etc/docker/daemon.json
