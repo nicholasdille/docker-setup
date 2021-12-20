109840aadbe64309bc3dd6b52e86baa462bf839a
Nicholas Dille <nicholas.dille@mailbox.org>
2021-12-20 18:08:32 +0100

    Test for working Docker daemon #117

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 curl -sLO https://github.com/nicholasdille/docker-setup/releases/latest/download/docker-setup.sh
-bash docker-setup.sh --no-wait
+bash docker-setup.sh --no-wait --simple-output --no-spinner
+docker version
