2edf68e79b301c1c1ba64aabc277b9f01347d1d6
Nicholas Dille <nicholas.dille@mailbox.org>
2022-02-01 11:49:08 +0100

    Testing changes to configuration for containerd

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -o errexit
 
-bash /docker-setup.sh --no-wait --no-progressbar --only docker jwt containerd crun gvisor ipfs imgcrypt dasel
+bash /docker-setup.sh --no-wait --no-progressbar \
+    --only \
+        dasel \
+        docker \
+        jwt \
+        containerd \
+        ipfs \
+        imgcrypt \
+        fuse-overlayfs-snapshotter \
+        stargz-snapshotter
 #bash /docker-setup.sh --check
