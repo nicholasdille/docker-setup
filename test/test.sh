f7aa671bbaaf08513dcd951941338ec0e565aff3
Nicholas Dille <nicholas.dille@mailbox.org>
2022-02-01 12:01:20 +0100

    Reverted to testing all tools

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 set -o errexit
 
-bash /docker-setup.sh --no-wait --no-progressbar \
-    --only \
-        dasel \
-        docker \
-        jwt \
-        containerd \
-        ipfs \
-        imgcrypt \
-        fuse-overlayfs-snapshotter \
-        stargz-snapshotter
-#bash /docker-setup.sh --check
+bash /docker-setup.sh --no-wait --no-progressbar
+bash /docker-setup.sh --check
