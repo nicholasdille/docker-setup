0b0c3c7ce7b2315680c31cf7d9d3a3bfc177bd7a
Nicholas Dille <nicholas.dille@mailbox.org>
2022-03-07 15:35:53 +0100

    Example tool definition #22 #410

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! test "$(uname -s)" == "Linux"; then
+    echo "ERROR: Unsupport operating system ($(uname -s))."
+    exit 1
+fi
+
 # TODO: Install yq
 YQ=yq
 TOOLS_YAML=tools.yaml
@@ -13,7 +18,18 @@ echo "tools(${#tools[@]}): ${tools[*]}."
 
 arch="$(uname -m)"
 echo "arch: ${arch}."
-# TODO: Set alt_arch to amd64/arm64
+case "${arch}" in
+    x86_64)
+        alt_arch=amd64
+        ;;
+    aarch64)
+        alt_arch=arm64
+        ;;
+    *)
+        echo "ERROR: Unsupported architecture (${arch})."
+        exit 1
+        ;;
+esac
 
 for tool in "${tools[@]}"; do
 
@@ -26,21 +42,30 @@ for tool in "${tools[@]}"; do
     # TODO: Install deps (deduplicate using hash tool_instlled)
 
     # TODO: Version check
+    # TODO: If .check is empty, use touch-based version in cache directory
+    # TODO: Substitute ${binary}
 
-    # TODO: Substitute arch/alt_arch
     location="$(
         arch="${arch}" ${YQ} eval '.[env(arch)]' <<<"${data}" \
-        | version="${version}" sed "s/\${version}/${version}/g"
+        | version="${version}" sed "s/\${version}/${version}/g" \
+        | arch="${arch}" sed "s/\${arch}/${arch}/g" \
+        | alt_arch="${alt_arch}" sed "s/\${alt_arch}/${alt_arch}/g"
     )"
     echo "  location: ${location}."
 
+    # TODO: Support type(.install)==string for custom installations
     install_type="$(${YQ} eval '.install.type' <<<"${data}")"
     echo "  type: ${install_type}."
 
     case "${install_type}" in
         executable)
-            # TODO: Substitute arch/alt_arch
-            binary="$(${YQ} eval '.binary' <<<"${data}")"
+            # TODO: Defaults to ${TARGET}/bin/${tool}
+            # TODO: Support paths relative to ${TARGET}/bin
+            binary="$(
+                ${YQ} eval '.binary' <<<"${data}" \
+                | arch="${arch}" sed "s/\${arch}/${arch}/g" \
+                | alt_arch="${alt_arch}" sed "s/\${alt_arch}/${alt_arch}/g"
+            )"
             echo "XXX curl -sLo ${binary} ${location}"
             echo "XXX chmod +x ${binary}"
             ;;
@@ -50,8 +75,11 @@ for tool in "${tools[@]}"; do
             if ! test "${strip_components}" == null; then
                 param_strip="--strip-components=${strip_components}"
             fi
-            # TODO: Substitute arch/alt_arch
-            install_files="$(${YQ} eval '.install.files[]' <<<"${data}")"
+            install_files="$(
+                ${YQ} eval '.install.files[]' <<<"${data}" \
+                | arch="${arch}" sed "s/\${arch}/${arch}/g" \
+                | alt_arch="${alt_arch}" sed "s/\${alt_arch}/${alt_arch}/g"
+            )"
             if ! test "${install_files}" == null; then
                 param_files="${install_files}"
             fi
@@ -66,10 +94,11 @@ for tool in "${tools[@]}"; do
             ;;
     esac
 
-    # TODO: Substitute arch/alt_arch
     post_install="$(
         ${YQ} eval '.post_install' <<<"${data}" \
-        | version="${version}" sed "s/\${version}/${version}/g"
+        | version="${version}" sed "s/\${version}/${version}/g" \
+        | arch="${arch}" sed "s/\${arch}/${arch}/g" \
+        | alt_arch="${alt_arch}" sed "s/\${alt_arch}/${alt_arch}/g"
     )"
     if ! test "${post_install}" == "null"; then
         echo "  post_install: ${post_install}."
