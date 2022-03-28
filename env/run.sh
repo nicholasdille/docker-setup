e3a69ff9cbb72098e956cb458cee91e66a3dcd9b
Nicholas Dille <nicholas.dille@mailbox.org>
2022-03-28 22:34:56 +0200

    Added alternative to colored output #558

--- env/run.sh
+++ env/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -o errexit
 
+echo "run.sh: TOOL=${TOOL}"
+
 if test -z "${TOOL}"; then
-    docker-setup --no-wait --no-progressbar --only docker
-    docker-setup --no-wait --no-progressbar
+    docker-setup --no-wait --no-color --no-progressbar --only docker
+    docker-setup --no-wait --no-color --no-progressbar
 
 else
-    docker-setup --no-wait --no-progressbar --only "${TOOL}"
+    docker-setup --no-wait --no-color --no-progressbar --only "${TOOL}"
 fi
 
-docker-setup --check --only-installed
+docker-setup --no-color --check --only-installed
