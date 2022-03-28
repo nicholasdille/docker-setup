9d06138a562e9dd06a6ddb04b83fdb51c8e256a4
Nicholas Dille <nicholas.dille@mailbox.org>
2022-03-28 22:25:36 +0200

    Attempt to improve speed of tests #558

--- env/run.sh
+++ env/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -o errexit
 
-docker-setup --no-wait --no-progressbar --only docker
-docker-setup --no-wait --no-progressbar
-docker-setup --check
+if test -z "${TOOL}"; then
+    docker-setup --no-wait --no-progressbar --only docker
+    docker-setup --no-wait --no-progressbar
+
+else
+    docker-setup --no-wait --no-progressbar --only "${TOOL}"
+fi
+
+docker-setup --check --only-installed
