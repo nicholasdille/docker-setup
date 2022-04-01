de482951c340f59f2f40eb65f2559578fe156833
Nicholas Dille <nicholas.dille@mailbox.org>
2022-04-01 14:42:58 +0200

    Backport of test speedup

--- env/run.sh
+++ env/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -o errexit
 
-docker-setup --no-wait --no-progressbar --only docker
-docker-setup --no-wait --no-progressbar
-docker-setup --check
+echo "run.sh: TOOL=${TOOL}"
+
+if test -z "${TOOL}"; then
+    docker-setup --no-wait --no-color --no-progressbar --only docker
+    docker-setup --no-wait --no-color --no-progressbar
+
+else
+    docker-setup --no-wait --no-color --no-progressbar --only "${TOOL}"
+fi
+
+docker-setup --no-color --check --only-installed
