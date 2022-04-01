cb99c63b704eccf5ef96a366f47c2eb054d234ce
Nicholas Dille <nicholas.dille@mailbox.org>
2022-04-01 22:49:41 +0200

    Removed useless comment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,6 @@ set -o errexit
 
 if ! test -S /var/run/docker.sock; then
     echo "WARNING: You must map the Docker socket for the installation."
-    # Add --no-docker?
 fi
 
 bash docker-setup.sh "$@"
