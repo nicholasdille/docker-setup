cbd2b1545c741366e2ca67a497a778f9d7bd20f7
Nicholas Dille <nicholas.dille@mailbox.org>
2022-07-05 11:49:27 +0200

    Fix automated test #1967

--- env/run.sh
+++ env/run.sh
@@ -6,9 +6,9 @@ echo "run.sh: TOOL=${TOOL}"
 if test -z "${TOOL}"; then
     docker-setup --no-wait --no-color --no-progressbar --only docker
     docker-setup --no-wait --no-color --no-progressbar
+    docker-setup --no-wait --no-color --no-progressbar --check
 
 else
     docker-setup --no-wait --no-color --no-progressbar --only "${TOOL}"
+    docker-setup --no-wait --no-color --no-progressbar --only "${TOOL}" --check
 fi
-
-docker-setup --no-color --check --only-installed
