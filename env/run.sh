fb3531c079a0b64a805589d52ca7ba85037daa58
Nicholas Dille <nicholas.dille@mailbox.org>
2022-07-05 13:01:11 +0200

    Fixed testing #1967

--- env/run.sh
+++ env/run.sh
@@ -5,10 +5,10 @@ echo "run.sh: TOOL=${TOOL}"
 
 if test -z "${TOOL}"; then
     docker-setup --no-wait --no-color --no-progressbar --only docker
-    docker-setup --no-wait --no-color --no-progressbar
+    docker-setup --no-wait --no-color --no-progressbar --all
+    docker-setup --no-wait --no-color --no-progressbar --check
 
 else
     docker-setup --no-wait --no-color --no-progressbar --only "${TOOL}"
+    docker-setup --no-wait --no-color --no-progressbar --only "${TOOL}" --check
 fi
-
-docker-setup --no-color --check --only-installed
