70f0674e0ef2cc025633e3fc8bcbf40214ecdb05
Nicholas Dille <nicholas.dille@mailbox.org>
2022-08-01 16:21:22 +0200

    Added support for multi-arch

--- trivy.sh
+++ trivy.sh
@@ -1,3 +1,13 @@
-docker build . --file Dockerfile.base          --tag ghcr.io/nicholasdille/docker-setup/base:oras         --push
-docker build . --file Dockerfile.trivy-build   --tag ghcr.io/nicholasdille/docker-setup/trivy:oras-0.30.4 --push
-docker build . --file Dockerfile.trivy-install                                                            --output type=local,dest=.
+#!/bin/bash
+set -o errexit
+
+if ! docker buildx ls | grep "^docker-setup "; then
+    docker buildx create --name docker-setup --bootstrap --platform linux/amd64,linux/arm64
+fi
+docker buildx use docker-setup
+
+docker build . --file Dockerfile.base          --tag ghcr.io/nicholasdille/docker-setup/base:oras         --platform linux/amd64,linux/arm64 --push
+docker build . --file Dockerfile.trivy-build   --tag ghcr.io/nicholasdille/docker-setup/trivy:oras-0.30.4 --platform linux/amd64,linux/arm64 --push
+docker build . --file Dockerfile.trivy-image   --tag ghcr.io/nicholasdille/docker-setup/final:oras        --platform linux/amd64,linux/arm64 --push
+docker build . --file Dockerfile.trivy-install                                                            --platform linux/amd64             --output type=local,dest=amd64
+docker build . --file Dockerfile.trivy-install                                                            --platform linux/arm64             --output type=local,dest=arm64
