59dc024eb9734bc40b216e0440d310d8faf25e4e
Nicholas Dille <nicholas.dille@mailbox.org>
2022-09-01 12:30:09 +0200

    Added generation of renovate config #2983

--- scripts/renovate.sh
+++ scripts/renovate.sh
@@ -4,7 +4,7 @@ set -o errexit
 cp renovate-root.json renovate.json
 jq --raw-output '.tools[] | select(.renovate != null) | .name' tools.json \
 | while read TOOL; do
-    jq --arg tool "${TOOL}" '.tools[] | select(.name == $tool) | .renovate | {"regexManagers": [{"fileMatch": ["^tools.yaml$"], "matchStrings": ["name: " + $tool + "\\n\\s+version: \"?(?<currentValue>.*?)\"?\\n"], "depNameTemplate": .package, "datasourceTemplate": .datasource, "extractVersionTemplate": .extractVersion, "versioningTemplate": .versioning}]}' tools.json \
+    jq --arg tool "${TOOL}" '.tools[] | select(.name == $tool) | .renovate | {"regexManagers": [{"fileMatch": ["^tools/" + $tool + "/manifest.yaml$"], "matchStrings": ["version: \"?(?<currentValue>.*?)\"?\\n"], "depNameTemplate": .package, "datasourceTemplate": .datasource, "extractVersionTemplate": .extractVersion, "versioningTemplate": .versioning}]}' tools.json \
     | jq 'if (.regexManagers[0].extractVersionTemplate == null) then del(.regexManagers[0].extractVersionTemplate) else . end' \
     | jq 'if (.regexManagers[0].versioningTemplate == null) then del(.regexManagers[0].versioningTemplate) else . end' \
     | jq --slurp '.[0].regexManagers += .[1].regexManagers | .[0]' renovate.json - >renovate.json.tmp
