23f6d80f6d88b80f09b4f18650706f84ab08a630
Nicholas Dille <nicholas.dille@mailbox.org>
2022-09-06 09:27:48 +0200

    Added tools 12/ #2983

---
+++ tools/portainer/post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit
+
+echo "Patch init script"
+sed -i "s|/usr/local/bin/portainer|${target}/bin/portainer|g" "${prefix}/etc/init.d/portainer"
+
+echo "Patch systemd unit"
+sed -i "s|/usr/local/bin/portainer|${target}/bin/portainer|g" "${prefix}/etc/systemd/system/portainer.service"
+
+if test -z "${prefix}" && has_systemd; then
+    echo "Reload systemd"
+    systemctl daemon-reload
+fi
+
