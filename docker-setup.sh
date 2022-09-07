e0a7e8625db14957258e95d5149b7d298a024dce
Nicholas Dille <nicholas.dille@mailbox.org>
2022-09-07 15:00:08 +0200

    More make improvements #2983

--- docker-setup.sh
+++ docker-setup.sh
@@ -8,6 +8,21 @@ fi
 
 : "${docker_setup_version:=oras}"
 
+cat <<"EOF"
+     _            _                           _
+  __| | ___   ___| | _____ _ __      ___  ___| |_ _   _ _ __
+ / _` |/ _ \ / __| |/ / _ \ '__|____/ __|/ _ \ __| | | | '_ \
+| (_| | (_) | (__|   <  __/ | |_____\__ \  __/ |_| |_| | |_) |
+ \__,_|\___/ \___|_|\_\___|_|       |___/\___|\__|\__,_| .__/
+                                                       |_|
+                     The container tools installer and updater
+                 https://github.com/nicholasdille/docker-setup
+--------------------------------------------------------------
+This script will install Docker Engine as well as useful tools
+from the container ecosystem.
+
+EOF
+
 docker_setup_tools_file="${docker_setup_cache}/tools.json"
 if test -f "${PWD}/tools.json"; then
     docker_setup_tools_file="${PWD}/tools.json"
