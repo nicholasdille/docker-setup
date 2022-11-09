f5a096bc41759df83f02c917215d93796405d6ab
Nicholas Dille <nicholas.dille@mailbox.org>
2022-11-09 10:00:50 +0100

    Fixed update of daemon.json

--- tools/crun/post_install.sh
+++ tools/crun/post_install.sh
@@ -3,8 +3,7 @@ set -o errexit
 
 if ! test -f "/etc/docker/daemon.json" || ! test "$(jq --raw-output '.runtimes | keys | any(. == "crun")' "/etc/docker/daemon.json")" == "true"; then
     echo "Add runtime to Docker"
-    # shellcheck disable=SC2094
-    cat >"${docker_setup_cache}/daemon.json-crun.sh" <<EOF
-cat <<< "\$(jq --arg target "${target}" '. * {"runtimes":{"crun":{"path":"\(\$target)/bin/crun"}}}' "/etc/docker/daemon.json")" >"/etc/docker/daemon.json"
-EOF
+    cat <<< "$(
+        jq --arg target "${target}" '. * {"runtimes":{"crun":{"path":"\($target)/bin/crun"}}}' /etc/docker/daemon.json
+    )" >/etc/docker/daemon.json
 fi
--- tools/gvisor/post_install.sh
+++ tools/gvisor/post_install.sh
@@ -3,8 +3,7 @@ set -o errexit
 
 if ! test -f "/etc/docker/daemon.json" || ! test "$(jq --raw-output '.runtimes | keys | any(. == "runsc")' "/etc/docker/daemon.json")" == "true"; then
     echo "Add runtime to Docker"
-    # shellcheck disable=SC2094
-    cat >"${docker_setup_cache}/daemon.json-gvisor.sh" <<EOF
-cat <<< "\$(jq --arg target "${target}" '. * {"runtimes":{"runsc":{"path":"\(\$target)/bin/runsc"}}}' "/etc/docker/daemon.json")" >"/etc/docker/daemon.json"
-EOF
+    cat <<< "$(
+        jq --arg target "${target}" '. * {"runtimes":{"runsc":{"path":"\($target)/bin/runsc"}}}' /etc/docker/daemon.json
+    )" >/etc/docker/daemon.json
 fi
