77fdd2a27163ee64d1c3e2f86689e2ad10dd2859
Nicholas Dille <nicholas.dille@mailbox.org>
2022-12-07 13:04:47 +0100

    Fixed usage of helper binaries #4024

--- make/site.mk
+++ make/site.mk
@@ -1,7 +1,5 @@
 site: $(HELPER)/var/lib/docker-setup/manifests/hugo.json $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW)))
-	@\
-	cd site; \
-	hugo --minify
+	@hugo --source site --minify
 
 $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW))):site/content/tools/%.md: scripts/gh-pages.sh tools/%/manifest.json ; $(info $(M) Generating static page for $*...)
 	@\
