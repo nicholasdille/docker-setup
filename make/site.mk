160520a4bbebf56214d5d60948ac0c0e06c1c531
Nicholas Dille <nicholas.dille@mailbox.org>
2022-12-07 12:46:44 +0100

    Fixes for #4024

--- make/site.mk
+++ make/site.mk
@@ -1,6 +1,6 @@
 site: $(HELPER)/var/lib/docker-setup/manifests/hugo.json $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW)))
 
-$(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW))):site/content/tools/%.md: tools/%/manifest.json ; $(info $(M) Generating static page for $*...)
+$(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW))):site/content/tools/%.md: scripts/gh-pages.sh tools/%/manifest.json ; $(info $(M) Generating static page for $*...)
 	@\
 	mkdir -p site/content/tools; \
 	bash scripts/gh-pages.sh "$*" >$@
