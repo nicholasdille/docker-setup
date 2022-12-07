32d4887940047b6a459f387df2d2d57ffe7f4a9c
Nicholas Dille <nicholas.dille@mailbox.org>
2022-12-07 12:58:24 +0100

    Fixed generation commands #4024

--- make/site.mk
+++ make/site.mk
@@ -1,4 +1,7 @@
 site: $(HELPER)/var/lib/docker-setup/manifests/hugo.json $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW)))
+	@\
+	cd site; \
+	hugo --minify
 
 $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW))):site/content/tools/%.md: scripts/gh-pages.sh tools/%/manifest.json ; $(info $(M) Generating static page for $*...)
 	@\
