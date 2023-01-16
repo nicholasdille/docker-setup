884e2a3d188cc6ec85debb0c62295821e9f256e1
Nicholas Dille <nicholas.dille@mailbox.org>
2023-01-16 10:10:09 +0100

    Added renovate for datasource git-tags #4426

--- scripts/renovate.sh
+++ scripts/renovate.sh
@@ -5,7 +5,25 @@ cp renovate-root.json renovate.json
 jq --raw-output '.tools[] | select(.renovate != null) | .name' metadata.json \
 | sort \
 | while read TOOL; do
-    jq --arg tool "${TOOL}" '.tools[] | select(.name == $tool) | .renovate | {"regexManagers": [{"fileMatch": ["^tools/" + $tool + "/manifest.yaml$"], "matchStrings": ["version: \"?(?<currentValue>.*?)\"?\\n"], "depNameTemplate": .package, "datasourceTemplate": .datasource, "extractVersionTemplate": .extractVersion, "versioningTemplate": .versioning}]}' metadata.json \
+    jq --arg tool "${TOOL}" '
+        .tools[] | 
+        select(.name == $tool) | 
+        .renovate | 
+        {
+            "regexManagers": [
+                {
+                    "fileMatch": [ "^tools/" + $tool + "/manifest.yaml$"],
+                    "matchStrings": [ "version: \"?(?<currentValue>.*?)\"?\\n" ],
+                    "packageNameTemplate": .url,
+                    "depNameTemplate": .package,
+                    "datasourceTemplate": .datasource,
+                    "extractVersionTemplate": .extractVersion,
+                    "versioningTemplate": .versioning
+                }
+            ]
+        }
+    ' metadata.json \
+    | jq 'if (.regexManagers[0].packageNameTemplate == null) then del(.regexManagers[0].packageNameTemplate) else . end' \
     | jq 'if (.regexManagers[0].extractVersionTemplate == null) then del(.regexManagers[0].extractVersionTemplate) else . end' \
     | jq 'if (.regexManagers[0].versioningTemplate == null) then del(.regexManagers[0].versioningTemplate) else . end' \
     | jq --slurp '.[0].regexManagers += .[1].regexManagers | .[0]' renovate.json - >renovate.json.tmp
