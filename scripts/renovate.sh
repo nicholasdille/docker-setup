c0c240792dab7626e06804d1d72e3707adc38806
Nicholas Dille <nicholas.dille@mailbox.org>
2023-01-16 15:44:05 +0100

    Improved performance when generating renovate.json

--- scripts/renovate.sh
+++ scripts/renovate.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 set -o errexit
 
-cp renovate-root.json renovate.json
-jq --raw-output '.tools[] | select(.renovate != null) | .name' metadata.json \
-| sort \
-| while read TOOL; do
-    jq --arg tool "${TOOL}" '
-        .tools[] | 
-        select(.name == $tool) | 
-        .renovate | 
-        {
-            "regexManagers": [
+jq '
+    {
+        "regexManagers": [
+            .tools[] |
+            if .renovate != null then
                 {
-                    "fileMatch": [ "^tools/" + $tool + "/manifest.yaml$"],
+                    "fileMatch": [ "^tools/" + .name + "/manifest.yaml$" ],
                     "matchStrings": [ "version: \"?(?<currentValue>.*?)\"?\\n" ],
-                    "packageNameTemplate": .url,
-                    "depNameTemplate": .package,
-                    "datasourceTemplate": .datasource,
-                    "extractVersionTemplate": .extractVersion,
-                    "versioningTemplate": .versioning
+                    "depNameTemplate": .renovate.package,
+                    "datasourceTemplate": .renovate.datasource,
+                    "packageNameTemplate": .renovate.url,
+                    "extractVersionTemplate": .renovate.extractVersion,
+                    "versioningTemplate": .renovate.versioning
                 }
-            ]
-        }
-    ' metadata.json \
-    | jq 'if (.regexManagers[0].packageNameTemplate == null) then del(.regexManagers[0].packageNameTemplate) else . end' \
-    | jq 'if (.regexManagers[0].extractVersionTemplate == null) then del(.regexManagers[0].extractVersionTemplate) else . end' \
-    | jq 'if (.regexManagers[0].versioningTemplate == null) then del(.regexManagers[0].versioningTemplate) else . end' \
-    | jq --slurp '.[0].regexManagers += .[1].regexManagers | .[0]' renovate.json - >renovate.json.tmp
-    mv renovate.json.tmp renovate.json
-done
+                |
+                if .packageNameTemplate == null then
+                    del(.packageNameTemplate)
+                else
+                    .
+                end
+                |
+                if .extractVersionTemplate == null then
+                    del(.extractVersionTemplate)
+                else
+                    .
+                end
+                |
+                if .versioningTemplate == null then
+                    del(.versioningTemplate)
+                else
+                    .
+                end
+
+            else
+                empty
+            end
+        ]
+    }
+' metadata.json \
+| jq --slurp '.[0].regexManagers += .[1].regexManagers | .[0]' renovate-root.json - \
+>renovate.json
