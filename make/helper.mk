bbbec1f2220cda141acbc56cd5ce8f16cf040db6
Nicholas Dille <nicholas.dille@mailbox.org>
2023-01-25 13:35:54 +0100

    Fixed helper tools

--- make/helper.mk
+++ make/helper.mk
@@ -1,5 +1,6 @@
 $(HELPER)/var/lib/docker-setup/manifests/%.json: $(HELPER)/var/lib/docker-setup/manifests/regclient.json
-	@docker_setup_cache="$${PWD}/cache" ./docker-setup --tools=$* --prefix=$(HELPER) install | cat
+	@docker_setup_cache="$${PWD}/cache" ./docker-setup --tools=$* --prefix=$(HELPER) install | cat; \
+	touch $(HELPER)/var/lib/docker-setup/manifests/$*.json
 
 $(HELPER)/var/lib/docker-setup/manifests/regclient.json $(HELPER)/var/lib/docker-setup/manifests/jq.json:
 	@set -o errexit; \
@@ -24,4 +25,6 @@ $(HELPER)/var/lib/docker-setup/manifests/regclient.json $(HELPER)/var/lib/docker
         gojq_v$${GOJQ_VERSION}_linux_amd64/gojq; \
 	mv $(HELPER)/usr/bin/gojq $(HELPER)/usr/bin/jq; \
 	chmod +x "$(HELPER)/usr/bin/regctl" "$(HELPER)/usr/bin/jq"; \
-	PATH="$(HELPER)/usr/bin:$${PATH}" docker_setup_cache="$${PWD}/cache" ./docker-setup --tools=regclient,jq --prefix=$(HELPER) install | cat
+	touch \
+		$(HELPER)/var/lib/docker-setup/manifests/regclient.json \
+		$(HELPER)/var/lib/docker-setup/manifests/jq.json
