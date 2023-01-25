53a18ca6840fa1660f1e94522a46bd8c452c22bf
Nicholas Dille <nicholas.dille@mailbox.org>
2023-01-25 15:58:38 +0100

    Fixed path to helper binaries

--- make/helper.mk
+++ make/helper.mk
@@ -16,15 +16,15 @@ $(HELPER)/var/lib/docker-setup/manifests/regclient.json $(HELPER)/var/lib/docker
 			echo ""; \
 			exit 1; \
 	esac; \
-	mkdir -p $(HELPER)/usr/bin $(HELPER)/usr/local/bin $(HELPER)/var/lib/docker-setup/manifests; \
-	curl --silent --location --fail --output "$(HELPER)/usr/bin/regctl" \
+	mkdir -p $(HELPER)/usr/local/bin $(HELPER)/usr/local/bin $(HELPER)/var/lib/docker-setup/manifests; \
+	curl --silent --location --fail --output "$(HELPER)/usr/local/bin/regctl" \
 		"https://github.com/regclient/regclient/releases/latest/download/regctl-linux-$${alt_arch}"; \
 	GOJQ_VERSION=0.12.11; \
 	curl --silent --location --fail "https://github.com/itchyny/gojq/releases/download/v$${GOJQ_VERSION}/gojq_v$${GOJQ_VERSION}_linux_$${alt_arch}.tar.gz" \
-    | tar --extract --gzip --directory="$(HELPER)/usr/bin" --strip-components=1 \
+    | tar --extract --gzip --directory="$(HELPER)/usr/local/bin" --strip-components=1 \
         gojq_v$${GOJQ_VERSION}_linux_amd64/gojq; \
-	mv $(HELPER)/usr/bin/gojq $(HELPER)/usr/bin/jq; \
-	chmod +x "$(HELPER)/usr/bin/regctl" "$(HELPER)/usr/bin/jq"; \
+	mv $(HELPER)/usr/local/bin/gojq $(HELPER)/usr/local/bin/jq; \
+	chmod +x "$(HELPER)/usr/local/bin/regctl" "$(HELPER)/usr/local/bin/jq"; \
 	touch \
 		$(HELPER)/var/lib/docker-setup/manifests/regclient.json \
 		$(HELPER)/var/lib/docker-setup/manifests/jq.json
