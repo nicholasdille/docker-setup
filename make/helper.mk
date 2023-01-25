c588797c11381d7a9c933280326cd9303a2d4ed2
Nicholas Dille <nicholas.dille@mailbox.org>
2023-01-25 11:11:03 +0100

    Migrated to multi-arch helpers

--- make/helper.mk
+++ make/helper.mk
@@ -3,8 +3,25 @@ $(HELPER)/var/lib/docker-setup/manifests/%.json: $(HELPER)/var/lib/docker-setup/
 
 $(HELPER)/var/lib/docker-setup/manifests/regclient.json $(HELPER)/var/lib/docker-setup/manifests/jq.json:
 	@set -o errexit; \
+	export arch="$$(uname -m)"; \
+	case "$${arch}" in \
+		x86_64) \
+			export alt_arch=amd64; \
+			;; \
+		aarch64) \
+			export alt_arch=arm64; \
+			;; \
+		*) \
+			echo ""; \
+			exit 1; \
+	esac; \
 	mkdir -p $(HELPER)/usr/bin $(HELPER)/usr/local/bin $(HELPER)/var/lib/docker-setup/manifests; \
-	curl --silent --location --output "$(HELPER)/usr/bin/regctl" "https://github.com/regclient/regclient/releases/latest/download/regctl-linux-amd64"; \
-	curl --silent --location --output "$(HELPER)/usr/bin/jq" "https://github.com/stedolan/jq/releases/latest/download/jq-linux64"; \
+	curl --silent --location --fail --output "$(HELPER)/usr/bin/regctl" \
+		"https://github.com/regclient/regclient/releases/latest/download/regctl-linux-$${alt_arch}"; \
+	GOJQ_VERSION=0.12.11; \
+	curl --silent --location --fail "https://github.com/itchyny/gojq/releases/download/v$${GOJQ_VERSION}/gojq_v$${GOJQ_VERSION}_linux_$${alt_arch}.tar.gz" \
+    | tar --extract --gzip --directory="$(HELPER)/usr/bin" --strip-components=1 \
+        gojq_v$${GOJQ_VERSION}_linux_amd64/gojq; \
+	mv $(HELPER)/usr/bin/gojq $(HELPER)/usr/bin/jq; \
 	chmod +x "$(HELPER)/usr/bin/regctl" "$(HELPER)/usr/bin/jq"; \
 	PATH="$(HELPER)/usr/bin:$${PATH}" docker_setup_cache="$${PWD}/cache" ./docker-setup --tools=regclient,jq --prefix=$(HELPER) install | cat
