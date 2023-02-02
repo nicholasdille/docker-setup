ed81fd50582dac25108876c6204989c0e6177037
Nicholas Dille <nicholas.dille@mailbox.org>
2023-02-02 18:11:19 +0100

    Improved remote targets

--- make/hcloud.mk
+++ make/hcloud.mk
@@ -6,7 +6,14 @@ HCLOUD_SERVER_TYPE ?= cx41
 DOCKER_CONFIG      ?= $(HOME)/.docker
 
 .PHONY:
-deploy-hcloud: vm-hcloud ~/..ssh/config.d/docker-setup vm-setup vm-install-docker vm-install-creds
+deploy-hcloud: vm-hcloud vm-wait-ready ~/.ssh/config.d/docker-setup vm-install-ds vm-install-docker vm-install-creds
+
+.PHONY:
+remove-hcloud: $(HELPER)/var/lib/docker-setup/manifests/hcloud.json $(HELPER)/var/lib/docker-setup/manifests/jq.json ; $(info $(M) Removing VM...)
+	@\
+	hcloud server list --selector purpose=docker-setup --output json \
+	| jq --raw-output '.[].id' \
+	| xargs hcloud server delete 
 
 .PHONY:
 vm-hcloud: $(HELPER)/var/lib/docker-setup/manifests/hcloud.json $(HELPER)/var/lib/docker-setup/manifests/jq.json ; $(info $(M) Creating VM...)
@@ -24,6 +31,13 @@ vm-hcloud: $(HELPER)/var/lib/docker-setup/manifests/hcloud.json $(HELPER)/var/li
 			--user-data-from-file contrib/cloud-init.yaml; \
 	fi
 
+.PHONY:
+vm-wait-ready: ~/.ssh/config.d/docker-setup ; $(info $(M) Waiting for VM to be ready...)
+	@\
+	while ! ssh docker-setup -- true; do \
+		sleep 10; \
+	done
+
 ~/.ssh/config.d/docker-setup: ~/.ssh/id_ed25519_hetzner $(HELPER)/var/lib/docker-setup/manifests/hcloud.json $(HELPER)/var/lib/docker-setup/manifests/jq.json ; $(info $(M) Creating SSH config for VM...)
 	@\
 	HCLOUD_VM_IP=$$(hcloud server list --selector purpose=docker-setup --output json | jq --raw-output 'select(. != null) |.[].public_net.ipv4.ip'); \
@@ -37,23 +51,17 @@ vm-hcloud: $(HELPER)/var/lib/docker-setup/manifests/hcloud.json $(HELPER)/var/li
     chmod 0600 $@
 
 .PHONY:
-vm-setup: ; $(info $(M) Preparing VM...)
+vm-install-ds: ; $(info $(M) Installing docker-setup...)
 	@\
-	ssh docker-setup rm -rf /docker-setup; \
-	ssh docker-setup git clone https://github.com/nicholasdille/docker-setup /docker-setup
+	scp docker-setup docker-setup:/usr/local/bin/docker-setup
 
 .PHONY:
-vm-install-ds:
-	@\
-	ssh docker-setup make --directory=/docker-setup /usr/local/bin/docker-setup
-
-.PHONY:
-vm-install-docker:
+vm-install-docker: ; $(info $(M) Installing default tools...)
 	@\
 	ssh docker-setup docker-setup --default install
 
 .PHONY:
-vm-install-creds:
+vm-install-creds: ; $(info $(M) Transferring Docker credentials...)
 	@\
 	ssh docker-setup docker-setup --tools=gojq install; \
 	ssh docker-setup mkdir -p /root/.docker; \
