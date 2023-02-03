dbc3effeb88834ea72e74fcc363736a358dd7b8b
Nicholas Dille <nicholas.dille@mailbox.org>
2023-02-03 12:40:33 +0100

    More progress on version catalog #4849

--- scripts/version_catalog.sh
+++ scripts/version_catalog.sh
@@ -21,11 +21,10 @@ CATALOG_JSON="$(
     ' metadata.json
 )"
 
-git log --oneline \
-| head -n 10 \
+git log --oneline --no-abbrev-commit \
 | cut -d' ' -f1 \
 | while read -r commit_sha; do
-    echo "${commit_sha}"
+    #echo "${commit_sha}"
 
     tools="$(
         git log --name-only "${commit_sha}~..${commit_sha}" tools/*/manifest.yaml \
@@ -37,10 +36,19 @@ git log --oneline \
         version="$(git show "${commit_sha}:tools/${name}/manifest.yaml" | yq eval .version)"
 
         echo "${name} ${version}: ${commit_sha}"
-        #CATALOG_JSON="$(
-            jq --arg name "${name}" --arg version "${version}" --arg commit_sha "${commit_sha}" '
-                .tools[] | select(.name == $name) | .versions[] += {"version": "\($version)", "commit_sha": "\($commit_sha)"}
-            ' <<<"${CATALOG_JSON}"
-        #)"
+        if ! jq --exit-status --arg name "${name}" --arg version "${version}" '.tools[] | select(.name == $name) | .versions[] | select(.version == $version)' <<<"${CATALOG_JSON}" >/dev/null 2>&1; then
+            #CATALOG_JSON="$(
+                jq --arg name "${name}" --arg version "${version}" --arg commit_sha "${commit_sha}" '
+                    .tools[] |
+                    select(.name == $name) |
+                    .versions += [ {
+                        "version": $version,
+                        "commit_sha": $commit_sha
+                    } ]
+                ' <<<"${CATALOG_JSON}"
+            #)"
+
+            exit
+        fi
     done
 done
