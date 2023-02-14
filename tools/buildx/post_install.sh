9f497bd98730210be2e501d22cd7b6ed5383da6d
Nicholas Dille <nicholas.dille@mailbox.org>
2023-02-14 09:35:31 +0100

    Fixed buildx when missing user config

--- tools/buildx/post_install.sh
+++ tools/buildx/post_install.sh
@@ -3,6 +3,10 @@
 echo "Make buildx the default builder on login"
 cat >"/etc/profile.d/docker-buildx-install.sh" <<EOF
 #!/bin/bash
+mkdir -p "\${HOME}/.docker"
+if ! test -f "\${HOME}/.docker/config.json"; then
+    echo '{}' >"\${HOME}/.docker/config.json"
+fi
 cat <<< "\$(jq '. * {"aliases": {"builder": "buildx"}}' "\${HOME}/.docker/config.json")" >"\${HOME}/.docker/config.json"
 EOF
 
