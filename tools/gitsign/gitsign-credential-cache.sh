031d69045e827672f3185f4e31eb478a232a4a75
Nicholas Dille <nicholas.dille@mailbox.org>
2023-03-08 12:18:43 +0100

    Fixed path to socket for gitsign-credential-cache

--- tools/gitsign/gitsign-credential-cache.sh
+++ tools/gitsign/gitsign-credential-cache.sh
@@ -3,4 +3,4 @@
 if test -z "$(pidof gitsign-credential-cache)"; then
     "gitsign-credential-cache" >/dev/null 2>&1 &
 fi
-export GITSIGN_CREDENTIAL_CACHE="${HOME}/.cache/.sigstore/gitsign/cache.sock"
+export GITSIGN_CREDENTIAL_CACHE="${HOME}/.cache/sigstore/gitsign/cache.sock"
