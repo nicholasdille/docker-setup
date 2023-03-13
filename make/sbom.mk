c7e65b2c3bd905c516f7f2cb089446df4d8b0e45
Nicholas Dille <nicholas.dille@mailbox.org>
2023-03-13 17:12:51 +0100

    Improve sbom scanning

--- make/sbom.mk
+++ make/sbom.mk
@@ -25,16 +25,18 @@ $(addsuffix --sbom,$(ALL_TOOLS_RAW)):%--sbom: $(TOOLS_DIR)/%/sbom.json
 
 $(addsuffix /sbom.json,$(ALL_TOOLS)):$(TOOLS_DIR)/%/sbom.json: $(HELPER)/var/lib/docker-setup/manifests/syft.json $(TOOLS_DIR)/%/manifest.json $(TOOLS_DIR)/%/Dockerfile ; $(info $(M) Creating sbom for $*...)
 	@set -o errexit; \
-	syft packages --output cyclonedx-json --file $@ $(REGISTRY)/$(REPOSITORY_PREFIX)$*:$(DOCKER_TAG); \
+	docker buildx imagetools inspect $(REGISTRY)/$(REPOSITORY_PREFIX)$*:$(DOCKER_TAG) --format "{{ json .SBOM }}" \
+	| jq --arg arch "$(ALT_ARCH)" '."linux/\($$arch)".SPDX' \
+	>$(TOOLS_DIR)/$*/sbom.json; \
 	test -s $(TOOLS_DIR)/$*/sbom.json || rm $(TOOLS_DIR)/$*/sbom.json
 
 .PHONY:
 attest: $(addsuffix --attest,$(TOOLS_RAW))
 
 .PHONY:
-$(addsuffix --scan,$(ALL_TOOLS_RAW)):%--scan: $(HELPER)/var/lib/docker-setup/manifests/grype.json $(TOOLS_DIR)/%/sbom.json
+$(addsuffix --scan,$(ALL_TOOLS_RAW)):%--scan: $(HELPER)/var/lib/docker-setup/manifests/grype.json $(TOOLS_DIR)/%/sbom.json ; $(info $(M) Scanning sbom for $*...)
 	@set -o errexit; \
-	grype sbom:$(TOOLS_DIR)/$*/sbom.json --add-cpes-if-none --fail-on high --output table
+	grype sbom:$(TOOLS_DIR)/$*/sbom.json --add-cpes-if-none --output table
 
 .PHONY:
 $(addsuffix --attest,$(ALL_TOOLS_RAW)):%--attest: $(HELPER)/var/lib/docker-setup/manifests/cosign.json sbom/%.json cosign.key ; $(info $(M) Attesting sbom for $*...)
