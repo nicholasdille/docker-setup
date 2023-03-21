cec369bc4038903f0fc9cc72ee6799c0a2ab27e9
Nicholas Dille <nicholas.dille@mailbox.org>
2023-03-21 13:24:00 +0100

    Fixed installation of prerequisites for site generation

--- make/site.mk
+++ make/site.mk
@@ -2,7 +2,10 @@
 pages: $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW)))
 
 .PHONY:
-site: metadata.json $(HELPER)/var/lib/docker-setup/manifests/hugo.json pages
+site-prerequisites: $(HELPER)/var/lib/docker-setup/manifests/hugo.json
+
+.PHONY:
+site: metadata.json site-prerequisites pages
 	@hugo --source site --minify
 
 $(addprefix site/content/tools/,$(addsuffix .md,$(ALL_TOOLS_RAW))):site/content/tools/%.md: scripts/gh-pages.sh tools/%/manifest.json ; $(info $(M) Generating static page for $*...)
