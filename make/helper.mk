4f35ee9077398c7679dae8ee27be2f904d2eb324
Nicholas Dille <nicholas.dille@mailbox.org>
2023-03-27 00:14:17 +0200

    Using existing vars for arch

--- make/helper.mk
+++ make/helper.mk
@@ -4,23 +4,11 @@ $(HELPER)/var/lib/docker-setup/manifests/%.json: $(HELPER)/var/lib/docker-setup/
 
 $(HELPER)/var/lib/docker-setup/manifests/regclient.json $(HELPER)/var/lib/docker-setup/manifests/jq.json:
 	@set -o errexit; \
-	export arch="$$(uname -m)"; \
-	case "$${arch}" in \
-		x86_64) \
-			export alt_arch=amd64; \
-			;; \
-		aarch64) \
-			export alt_arch=arm64; \
-			;; \
-		*) \
-			echo ""; \
-			exit 1; \
-	esac; \
 	mkdir -p $(HELPER)/usr/local/bin $(HELPER)/usr/local/bin $(HELPER)/var/lib/docker-setup/manifests; \
 	curl --silent --location --fail --output "$(HELPER)/usr/local/bin/regctl" \
-		"https://github.com/regclient/regclient/releases/latest/download/regctl-linux-$${alt_arch}"; \
+		"https://github.com/regclient/regclient/releases/latest/download/regctl-linux-$(ALT_ARCH)"; \
 	GOJQ_VERSION=0.12.11; \
-	curl --silent --location --fail "https://github.com/itchyny/gojq/releases/download/v$${GOJQ_VERSION}/gojq_v$${GOJQ_VERSION}_linux_$${alt_arch}.tar.gz" \
+	curl --silent --location --fail "https://github.com/itchyny/gojq/releases/download/v$${GOJQ_VERSION}/gojq_v$${GOJQ_VERSION}_linux_$(ALT_ARCH).tar.gz" \
     | tar --extract --gzip --directory="$(HELPER)/usr/local/bin" --strip-components=1 \
         gojq_v$${GOJQ_VERSION}_linux_amd64/gojq; \
 	mv $(HELPER)/usr/local/bin/gojq $(HELPER)/usr/local/bin/jq; \
