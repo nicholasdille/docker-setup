22eeabd4a7b143b5d6a06113927e4d7c9a4fd07b
Nicholas Dille <nicholas.dille@mailbox.org>
2023-05-26 11:55:35 +0200

    Removed conditional symbolic links

--- tools/podman-docker-shim/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-if ! test -f "${target}/bin/docker"; then
-    echo "Installing docker shim for podman"
-    ln --symbolic --relative --force "${prefix}${target}/libexec/podman/docker" "${target}/bin/docker"
-fi
