26724ad29d0246cb3ec6af5d7add6b00cb985648
Nicholas Dille <nicholas.dille@mailbox.org>
2023-07-06 13:29:37 +0200

    Updated for #6270

--- tools/cri-dockerd/post_install.sh
+++ tools/cri-dockerd/post_install.sh
@@ -4,13 +4,16 @@ set -o errexit
 if test -f "/etc/crictl.yaml"; then
     echo "Fixing configuration for cticrl"
     ENDPOINT=unix:///var/run/cri-dockerd.sock
-    sed -i \
-        "s|#runtime-endpoint: YOUR-CHOICE|runtime-endpoint: ${ENDPOINT}|; s|#image-endpoint: YOUR-CHOICE|image-endpoint: ${ENDPOINT}|" \
-        "/etc/crictl.yaml"
+    cat "${target}/etc/crictl.yaml" \
+    | sed "s|#runtime-endpoint: YOUR-CHOICE|runtime-endpoint: ${ENDPOINT}|; s|#image-endpoint: YOUR-CHOICE|image-endpoint: ${ENDPOINT}|" \
+    >"/etc/crictl.yaml"
 fi
 
-echo "Patch systemd unit"
-sed -i "s|ExecStart=/usr/bin/cri-dockerd|ExecStart=${target}/bin/cri-dockerd|" "/etc/systemd/system/cri-docker.service"
+echo "Install systemd unit"
+cat "${target}/etc/systemd/system/cri-docker.service" \
+| sed "s|ExecStart=/usr/bin/cri-dockerd|ExecStart=${target}/bin/cri-dockerd|" \
+>"/etc/systemd/system/cri-docker.service"
+
 if systemctl >/dev/null 2>&1; then
     echo "Reload systemd"
     systemctl daemon-reload
