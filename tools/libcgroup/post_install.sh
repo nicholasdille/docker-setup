e5699b9c96c9e725a7625f88bd7c134b69ef0304
Nicholas Dille <nicholas.dille@mailbox.org>
2023-07-10 15:00:16 +0200

    Fixed tools after #6270

--- tools/libcgroup/post_install.sh
+++ tools/libcgroup/post_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -o errexit
 
+mkdir -p "/etc/ld.so.conf.d"
 cp "${target}/etc/ld.so.conf.d/libcgroup.conf" "/etc/ld.so.conf.d/libcgroup.conf"
 ldconfig
--- tools/runq/post_install.sh
+++ tools/runq/post_install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -o errexit
 
-cp -r "${target}/var/lib/" "/var/lib/"
-
-if ! test -f "/etc/docker/daemon.json" || ! test "$(jq --raw-output '.runtimes | keys | any(. == "crun")' "/etc/docker/daemon.json")" == "true"; then
+if ! test -f "/etc/docker/daemon.json" || ! test "$(jq --raw-output 'select(.runtimes != null) | .runtimes | keys | any(. == "runq")' "/etc/docker/daemon.json")" == "true"; then
     echo "Add runtime to Docker"
     cat <<< "$(
-        jq --arg target "${target}" '. * {"runtimes":{"runq":{"path":"\($target)/bin/runq", "runtimeArgs": [ 
-            "--cpu", "1",
-            "--mem", "256",
-            "--dns", "8.8.8.8,8.8.4.4",
-            "--tmpfs", "/tmp" 
-        ]}}}' /etc/docker/daemon.json
+        jq --arg target "${target}" '. * {
+            "runtimes": {
+                "runq": {
+                    "path": "\($target)/var/lib/runq/runq",
+                    "runtimeArgs": [
+                        "--cpu", "1",
+                        "--mem", "256",
+                        "--dns", "8.8.8.8,8.8.4.4",
+                        "--tmpfs", "/tmp" 
+                    ]
+                }
+            }}' /etc/docker/daemon.json
     )" >/etc/docker/daemon.json
 fi
