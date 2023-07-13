8d73bbab139715d7a168e73a2da9511cb11124f5
Nicholas Dille <nicholas.dille@mailbox.org>
2023-07-13 12:02:59 +0200

    Added check for github release asset

--- @base/check-github-release-asset.sh
+++ @base/check-github-release-asset.sh
@@ -24,11 +24,26 @@ function check-github-release-asset() {
 
     local url="https://github.com/${repo}/releases/download/${version}/${asset}"
     echo "### Checking ${repo} ${version} ${asset}"
-    if curl --silent --location --head --url "${url}"; then
+    if curl --silent --location --fail --head --url "${url}"; then
         echo "    Found :-)"
         return
     fi
-    echo "ERROR: Asset ${asset} not found for ${repo} ${version} at ${url}"
+    echo "    ERROR: Asset ${asset} not found for ${repo} ${version}"
+    echo "           at ${url}"
 
-    #
+    echo "### Fetching release assets for ${repo} ${version}"
+    local id
+    local url="https://api.github.com/repos/${repo}/releases/tags/${version}"
+    if ! id="$(curl --silent --location --fail "${url}" | jq --raw-output '.id')"; then
+        echo "    ERROR: Failed to fetch release id for ${repo} ${version}"
+        echo "           at ${url}"
+    fi
+    echo "    Found release id ${id} for ${repo} ${version}"
+
+    echo "### Fetching release assets for ${repo} ${version} with id ${id}"
+    local url="https://api.github.com/repos/${repo}/releases/${id}/assets"
+    if ! curl --silent --location --fail "${url}" | jq --raw-output '.[].name'; then
+        echo "    ERROR: Failed to fetch release assets for ${repo} ${version}"
+        echo "           at ${url}"
+    fi
 }
